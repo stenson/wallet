65990694f66374ed30298ac92413f39d79a83f8f
Robert Stenson <rob.stenson@gmail.com>
2011-04-30 16:41:12 -0700

    update the ender build in the test folder

--- test/node_modules/.npm/event-queue/0.1.0/package/engines/node/event-queue.js
+++
@@ -1,7 +0,0 @@
-'use strict'
-
-exports.setTimeout = setTimeout
-exports.setInterval = setInterval
-exports.clearTimeout = clearTimeout
-exports.clearInterval = clearTimeout
-exports.enqueue = process.nextTick
--- test/node_modules/.npm/event-queue/0.1.0/package/lib/event-queue.js
+++
@@ -1,8 +0,0 @@
-'use strict'
-
-const timer = require("timer")
-exports.setTimeout = timer.setTimeout
-exports.setInterval = timer.setInterval
-exports.clearTimeout = timer.clearTimeout
-exports.clearInterval = timer.clearInterval
-exports.enqueue = timer.setTimeout
