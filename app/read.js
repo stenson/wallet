97b23e65de3060a0141416fd97153097f40680a5
Robert Stenson <rob.stenson@gmail.com>
2011-05-10 14:20:01 -0700

    made it work with the latest ender

--- app/read.js
+++ app/read.js
@@ -5,55 +5,62 @@ var fs = require("fs"),
   ender = {
     get: require("ender/lib/ender.get"),
     file: require("ender/lib/ender.file"),
-    jeesh: require("ender/lib/ender.jeesh")
+    util: require("ender/lib/ender.util")
+    //jeesh: require("ender/lib/ender.jeesh")
   },
   // borrowed from ender
   commonJSBridge = { head: '!function () { var exports = {}, module = { exports: exports }; '
-                   , foot: ' $.ender(module.exports); }();' }
-
-// pass all files as data to the html page, then eval in the native webkit dom
-// so you can tell what is what by differentiation
+                   , foot: ' $.ender(module.exports); }();' },
+  FILE = ender.file,
+  UTIL = ender.util
 
 // had to copy the whole thing to get the names associated with scripts
-function processPackages(packages,_isAsync,callback) {
-  var _sync = [], _async = [], _asyncBridges = [], i = 0;
-  packages.forEach(function (name) {
-    if(!name) return;
-    var location = path.join('node_modules', name, 'package.json');
-    fs.readFile(location, 'utf-8', function (err, data) {
-      if (err) throw err;
-      var packageJSON = JSON.parse(data)
-      , isAsync = /ender\-js|scriptjs/.test(name) || !packageJSON.ender ? false : _isAsync
-      , source;
-      //CONSTRUCT MAIN SOURCE FILE
-      if (!packageJSON.main) {
-        return console.log(name + ' source not found');
-      } else if (typeof packageJSON.main == 'string') {
-        packageJSON.main = [ packageJSON.main ];
-      }
-      ender.file.constructSource(isAsync, _async, name, packageJSON.main, function (result) {
-        if (!isAsync) {
-          source = result;
-        } else {
-          _async = result;
-        }
-        //CONSTRUCT BRIDGE
-        ender.file.constructBridge(name, packageJSON.ender, function (content) {
-          if (name == 'ender-js') {
-            source = ender.file.processComment(source, "");
-          } else {
-            if (packageJSON.ender) {
-              isAsync ? _asyncBridges.push(content) : (source += content);
-            } else if (!isAsync) {
-              source = commonJSBridge.head + source + commonJSBridge.foot;
+function processPackages(_packages, context, callback) {
+  var result = [], i = 0;
+  FILE.constructDependencyTree(_packages, 'node_modules', function (tree) {
+    FILE.flattenDependencyTree(tree, null, function (packages) {
+      packages = UTIL.unique(packages);
+      packages.forEach(function (name, j) {
+        var packagePath = path.join('node_modules', name.replace(/\//g, '/node_modules/'))
+          , location = path.join(packagePath, 'package.json');
+        path.exists(location, function (exists) {
+          if (!exists) {
+            console.log('dependency ' + name.red + ' is currently not installed... for details check: ' + '$ ender info'.yellow);
+            if (packages.length == ++i) {
+               callback && callback(result);
             }
+            return;
           }
-          if (!isAsync) {
-            _sync.push([name,source,packageJSON]);
-          }
-          if (packages.length == ++i) {
-             callback && callback(_sync, _async, _asyncBridges);
-          }
+          fs.readFile(location, 'utf-8', function (err, data) {
+            if (err) throw err;
+            var packageJSON = JSON.parse(data)
+            , source;
+            //CONSTRUCT MAIN SOURCE FILE
+            if (!packageJSON.main) {
+              packageJSON.main = [];
+            } else if (typeof packageJSON.main == 'string') {
+              packageJSON.main = [ packageJSON.main ];
+            }
+            FILE.constructSource(packagePath, packageJSON.main, function (source) {
+              //CONSTRUCT BRIDGE
+              FILE.constructBridge(packagePath, packageJSON.ender, function (content) {
+                if (name == 'ender-js') {
+                  source = FILE.processComment(source, context);
+                } else {
+                  if (!packageJSON.ender) {
+                    source = commonJSBridge.head + source + commonJSBridge.foot;
+                  } else if (packageJSON.ender != 'noop'){
+                    source += content;
+                  }
+                }
+                // the one modified line
+                result[j] = [name,source,packageJSON];
+                if (packages.length == ++i) {
+                   callback && callback(result);
+                }
+              });
+            });
+          });
         });
       });
     });
@@ -64,9 +71,9 @@ module.exports = {
   read: function(callback) {
     ender.get.buildHistory(function(packages){
       var packs = packages.split(" ").slice(2);
-      if(packs[0] == "jeesh") {
-        packs = ender.jeesh;
-      }
+      // // if(packs[0] == "jeesh") {
+      // //   packs = ender.jeesh;
+      // // }
       if(packs[0] == "") {
         packs = [];
       }
@@ -85,6 +92,6 @@ module.exports = {
     ender.file.enderSize(function(size){
       callback(((Math.round((size/1024) * 10) / 10)));
     });
-  },
-  jeesh: ender.jeesh
+  }//,
+  //jeesh: ender.jeesh
 }
